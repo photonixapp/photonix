f813cfa8b252e9b6a5a94997e05a935225d37234
Damian Moore <damian@epixstudios.co.uk>
2019-01-10 23:04:39 +0000

    Fixes #17: Gunicorn rather than Django runserver when not in dev mode

---
+++ system/run_app_server.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+if [ "$ENV" = "dev" ]
+then
+  >&2 echo "Starting Django runserver as not in prd mode"
+  pipenv run python /srv/backend/manage.py runserver 0.0.0.0:8000
+else
+  >&2 echo "Starting Gunicorn server as in prd mode"
+  cd /srv/backend && pipenv run gunicorn -b 0.0.0.0:8000 web.wsgi
+fi
