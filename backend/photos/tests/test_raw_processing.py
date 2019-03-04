27786910cb6c5d0029cbaf9d23df382806c088ac
Damian Moore <damian@epixstudios.co.uk>
2019-03-04 07:45:15 +0000

    Attempt to fix tests on Travis

--- backend/photos/tests/test_raw_processing.py
+++ backend/photos/tests/test_raw_processing.py
@@ -7,14 +7,14 @@ from photos.utils.raw import generate_jpeg, process_ensure_raw_processed_tasks, 
 
 PHOTOS = [
     # -e argument to dcraw means JPEG was extracted without any processing
-    ('Adobe DNG Converter - Canon EOS 5D Mark III - Lossy JPEG compression (3_2).DNG',  'dcraw -e', '1236950, 'https://raw.pixls.us/getfile.php/1023/nice/Adobe%20DNG%20Converter%20-%20Canon%20EOS%205D%20Mark%20III%20-%20Lossy%20JPEG%20compression%20(3:2).DNG'),
-    ('Apple - iPhone 8 - 16bit (4_3).dng',                                              'dcraw -w', '772618, 'https://raw.pixls.us/getfile.php/2835/nice/Apple%20-%20iPhone%208%20-%2016bit%20(4:3).dng'),  # No embedded JPEG
-    ('Canon - Canon PowerShot SX20 IS.DNG',                                             'dcraw -w', '1828344, 'https://raw.pixls.us/getfile.php/861/nice/Canon%20-%20Canon%20PowerShot%20SX20%20IS.DNG'),  # Embedded image but low resolution and not a JPEG
-    ('Canon - EOS 7D - sRAW2 (sRAW) (3:2).CR2',                                         'dcraw -e', '2264602, 'https://raw.pixls.us/getfile.php/129/nice/Canon%20-%20EOS%207D%20-%20sRAW2%20(sRAW)%20(3:2).CR2'),
-    ('Canon - Powershot SX110IS - CHDK.CR2',                                            'dcraw -w', '1493825, 'https://raw.pixls.us/getfile.php/144/nice/Canon%20-%20Powershot%20SX110IS%20-%20CHDK.CR2'),  # No embedded JPEG, No metadata about image dimensions for us to compare against
-    ('Leica - D-LUX 5 - 16_9.RWL',                                                      'dcraw -w', '1478207, 'https://raw.pixls.us/getfile.php/2808/nice/Leica%20-%20D-LUX%205%20-%2016:9.RWL'),  # Less common aspect ratio, fairly large embedded JPEG but not similar enough to the raw's dimensions
-    ('Nikon - 1 J1 - 12bit compressed (Lossy (type 2)) (3_2).NEF',                      'dcraw -e', '635217, 'https://raw.pixls.us/getfile.php/2956/nice/Nikon%20-%201%20J1%20-%2012bit%20compressed%20(Lossy%20(type%202))%20(3:2).NEF'),
-    ('Sony - SLT-A77 - 12bit compressed (3_2).ARW',                                     'dcraw -w', '859814, 'https://raw.pixls.us/getfile.php/2691/nice/Sony%20-%20SLT-A77%20-%2012bit%20compressed%20(3:2).ARW'),  # Large embedded JPEG but not the right aspect ratio and smaller than raw
+    ('Adobe DNG Converter - Canon EOS 5D Mark III - Lossy JPEG compression (3_2).DNG',  'dcraw -e', 1236950, 'https://raw.pixls.us/getfile.php/1023/nice/Adobe%20DNG%20Converter%20-%20Canon%20EOS%205D%20Mark%20III%20-%20Lossy%20JPEG%20compression%20(3:2).DNG'),
+    ('Apple - iPhone 8 - 16bit (4_3).dng',                                              'dcraw -w', 772618, 'https://raw.pixls.us/getfile.php/2835/nice/Apple%20-%20iPhone%208%20-%2016bit%20(4:3).dng'),  # No embedded JPEG
+    ('Canon - Canon PowerShot SX20 IS.DNG',                                             'dcraw -w', 1828344, 'https://raw.pixls.us/getfile.php/861/nice/Canon%20-%20Canon%20PowerShot%20SX20%20IS.DNG'),  # Embedded image but low resolution and not a JPEG
+    ('Canon - EOS 7D - sRAW2 (sRAW) (3:2).CR2',                                         'dcraw -e', 2264602, 'https://raw.pixls.us/getfile.php/129/nice/Canon%20-%20EOS%207D%20-%20sRAW2%20(sRAW)%20(3:2).CR2'),
+    ('Canon - Powershot SX110IS - CHDK.CR2',                                            'dcraw -w', 1493825, 'https://raw.pixls.us/getfile.php/144/nice/Canon%20-%20Powershot%20SX110IS%20-%20CHDK.CR2'),  # No embedded JPEG, No metadata about image dimensions for us to compare against
+    ('Leica - D-LUX 5 - 16_9.RWL',                                                      'dcraw -w', 1478207, 'https://raw.pixls.us/getfile.php/2808/nice/Leica%20-%20D-LUX%205%20-%2016:9.RWL'),  # Less common aspect ratio, fairly large embedded JPEG but not similar enough to the raw's dimensions
+    ('Nikon - 1 J1 - 12bit compressed (Lossy (type 2)) (3_2).NEF',                      'dcraw -e', 635217, 'https://raw.pixls.us/getfile.php/2956/nice/Nikon%20-%201%20J1%20-%2012bit%20compressed%20(Lossy%20(type%202))%20(3:2).NEF'),
+    ('Sony - SLT-A77 - 12bit compressed (3_2).ARW',                                     'dcraw -w', 859814, 'https://raw.pixls.us/getfile.php/2691/nice/Sony%20-%20SLT-A77%20-%2012bit%20compressed%20(3:2).ARW'),  # Large embedded JPEG but not the right aspect ratio and smaller than raw
 ]
 
 
