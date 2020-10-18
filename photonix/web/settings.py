29f1249100cedc5dad1379e6d86ef0f53c095377
Damian Moore <damian@epixstudios.co.uk>
2020-10-18 13:08:21 +0100

    Fixes to static file handing across dev and prd Docker builds

--- photonix/web/settings.py
+++ photonix/web/settings.py
@@ -128,8 +128,8 @@ else:
 CACHE_DIR = str(Path(DATA_DIR) / 'cache')
 MODEL_DIR = str(Path(DATA_DIR) / 'models')
 
-STATIC_DIR = str(Path(BASE_DIR) / 'static')
-STATIC_URL = '/static/'
+STATIC_ROOT = '/srv/static-collected'
+STATIC_URL = '/static-collected/'
 
 MEDIA_ROOT = str(Path(BASE_DIR).parent / 'data')
 
