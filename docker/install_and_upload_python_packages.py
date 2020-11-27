08c77f0160031db81a417ef768e0754cf8d09cba
Damian Moore <damian@epixstudios.co.uk>
2020-11-27 22:07:46 +0000

    Uploading whl packages as installing via pip

--- docker/install_and_upload_python_packages.py
+++ docker/install_and_upload_python_packages.py
@@ -1,4 +1,8 @@
-from subprocess import Popen, PIPE
+import os
+import subprocess
+
+
+os.environ['PYTHONUNBUFFERED'] = '1'
 
 
 def install_and_upload():
@@ -7,28 +11,29 @@ def install_and_upload():
     # this script uploads built .whl packages to a private PyPI server so
     # they're cached for next time.
 
-    with open('/srv/requirements.txt') as f:
-        for line in f.readlines():
-            print(line.strip())
-            child = Popen(['pip', 'install', line], stdout=PIPE, stdin=PIPE, stderr=PIPE)
-            data = child.communicate()[0].decode('utf-8')
-            print(data)
-            if child.returncode != 0:
-                exit(1)
+    cmd = ['/usr/local/bin/pip', 'install', '-r', '/srv/requirements.txt']
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, env={'PYTHONUNBUFFERED': '1'})
+    for line in proc.stdout:
+        line = line.rstrip().decode("utf-8")
+        print(line)
 
-            if 'Created wheel' in data:
-                whl_name = None
-                for install_line in data.split('\n'):
-                    if 'Created wheel for' in install_line:
-                        whl_name = install_line.split('filename=')[1].split(' ')[0]
-                    if 'Stored in directory' in install_line:
-                        folder = install_line.split('Stored in directory:')[1].strip()
-                        child = Popen(['pypiupload', 'files', f'{folder}/{whl_name}', '-i', 'epix'], stdout=PIPE, stdin=PIPE, stderr=PIPE)
-                        data = child.communicate()[0].decode('utf-8')
-                        print(data)
-                        if child.returncode != 0:
-                            exit(1)
-                        print()
+        if 'Created wheel' in line:
+            whl_name = None
+            for install_line in line.split('\n'):
+                if 'Created wheel for' in install_line:
+                    whl_name = install_line.split('filename=')[1].split(' ')[0]
+                if 'Stored in directory' in install_line:
+                    folder = install_line.split('Stored in directory:')[1].strip()
+                    child = Popen(['pypiupload', 'files', f'{folder}/{whl_name}', '-i', 'epix'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, env={'PYTHONUNBUFFERED': '1'})
+                    for child_line in child.stdout:
+                        child_line = child_line.rstrip().decode("utf-8")
+                        print(child_line)
+                    if child.wait() != 0:
+                        exit(1)
+                    print()
+
+    if proc.wait() != 0:
+        exit(1)
 
 if __name__ == '__main__':
     install_and_upload()
