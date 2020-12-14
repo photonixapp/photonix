e4774dd43c7388bf7422b582006ebf848c04472f
Damian Moore <damian@epixstudios.co.uk>
2020-12-14 19:11:14 +0000

    Temporarily disabling upload URL

--- photonix/web/urls.py
+++ photonix/web/urls.py
@@ -9,6 +9,6 @@ from photonix.photos.views import thumbnail, upload
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql', csrf_exempt(jwt_cookie(GraphQLView.as_view(graphiql=True))), name='api'),  # TODO: Check that use of csrf_exempt is OK here because of CORS configuration
-    path('upload/', csrf_exempt(upload)),
+    # path('upload/', csrf_exempt(upload)),
     re_path('thumbnails/(?P<width>[0-9]+)x(?P<height>[0-9]+)_(?P<crop>cover|contain)_q(?P<quality>[0-9]+)/(?P<photo_id>[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12})/$', thumbnail),
 ]
