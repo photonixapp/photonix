965ab245741fc83453889033fc289e13e81c1ba0
Damian Moore <damian@epixstudios.co.uk>
2020-12-16 19:02:29 +0000

    Fixes Apollo authentication bug on search filters

--- ui/src/index.js
+++ ui/src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reducers from './stores'
 
-export const store = createStore(reducers);
+export const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <Provider store={store}>
