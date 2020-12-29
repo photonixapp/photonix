99ec25d9a2fe801f1a32031b4baeecef8ebc0e22
Gyan P <Gyanp@github.com>
2020-12-29 16:25:01 +0530

    conflict solved

--- photonix/accounts/schema.py
+++ photonix/accounts/schema.py
@@ -42,3 +42,41 @@ class Query(graphene.ObjectType):
             'demo': os.environ.get('DEMO', False),
             'first_run': False,
         }
+        user = User.objects.first()
+        if user and user.has_config_persional_info and \
+            user.has_created_library and user.has_configured_importing and \
+                user.has_configured_image_analysis:
+            # raise Exception(info.context.user.is_anonymous)
+            return {
+                'demo': os.environ.get('DEMO', False),
+                'first_run': False,
+            }
+        else:
+            if not user:
+                return {
+                    'demo': os.environ.get('DEMO', False), 'first_run': True,
+                    'form': 'has_config_persional_info'}
+            if not user.has_created_library:
+                return {
+                    'demo': os.environ.get('DEMO', False), 'first_run': True,
+                    'form': 'has_created_library', 'user_id': user.id}
+            if not user.has_configured_importing:
+                return {
+                    'demo': os.environ.get('DEMO', False), 'first_run': True,
+                    'form': 'has_configured_importing', 'user_id': user.id,
+                    'library_id': Library.objects.filter(users__user=user)[0].id,
+                    'library_path_id': LibraryPath.objects.filter(library__users__user=user)[0].id
+                }
+            if not user.has_configured_image_analysis:
+                return {
+                    'demo': os.environ.get('DEMO', False), 'first_run': True,
+                    'form': 'has_configured_image_analysis', 'user_id': user.id,
+                    'library_id': Library.objects.filter(users__user=user)[0].id,
+                }
+
+    def resolve_after_signup(self, info):
+        """To login user from frontend after finish sigunp process."""
+        user = info.context.user
+        if user.has_configured_image_analysis:
+            return {'token': get_token(user), 'refresh_token': create_refresh_token(user)}
+        return {'token': None, 'refresh_token': None}
