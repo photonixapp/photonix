33301663aaf62c4fcdffc3b9e71edd87fcaf545a
Damian Moore <damian@epixstudios.co.uk>
2021-02-06 12:02:47 +0000

    Improved color detection pallette and analysis

--- photonix/classifiers/color/model.py
+++ photonix/classifiers/color/model.py
@@ -9,7 +9,7 @@ from PIL import Image
 
 
 class ColorModel:
-    version = 20181130
+    version = 20210206
     approx_ram_mb = 120
     max_num_workers = 2
 
@@ -17,35 +17,29 @@ class ColorModel:
         self.colors = {
             # Name: ((red, green, blue), ordering)
 
-            # 'Red':                  (255, 0, 0),
-            # 'Yellow':               (255, 255, 0),
-            # 'Green':                (0, 255, 0),
-            # 'Cyan':                 (0, 255, 255),
-            # 'Blue':                 (0, 0, 255),
-            # 'Magenta':              (255, 0, 255),
+            'Red':                  ((120, 4, 20),     1),
+            'Orange':               ((245, 133, 0),     2),
+            'Amber':                ((234, 166, 30),    3),
+            'Yellow':               ((240, 240, 39),    4),
+            'Lime':                 ((168, 228, 26),    5),
+            'Green':                ((7, 215, 7),       6),
+            'Teal':                 ((16, 202, 155),    7),
+            'Turquoise':            ((25, 225, 225),    8),
+            'Aqua':                 ((10, 188, 245),    9),
+            'Azure':                ((30, 83, 249),     10),
+            'Blue':                 ((0, 0, 255),       11),
+            'Purple':               ((127, 0, 255),     12),
+            'Orchid':               ((190, 0, 255),     13),
+            'Magenta':              ((233, 8, 200),     14),
 
-            'Red':                  ((120, 4, 20),      1),
-            'Dark orange':          ((162, 70, 21),     2),
-            'Orange':               ((255, 124, 0),     3),
-            'Pale pink':            ((255, 159, 156),   4),
-            'Lemon yellow':         ((255, 250, 0),     5),
-            'School bus yellow':    ((255, 207, 0),     6),
-            'Green':                ((144, 226, 0),     7),
-            'Dark lime green':      ((0, 171, 0),       8),
-            'Cyan':                 ((0, 178, 212),     9),
-            'Blue':                 ((0, 98, 198),      10),
-            'Violet':               ((140, 32, 186),    11),
-            'Pink':                 ((245, 35, 148),    12),
-
-            'White':                ((255, 255, 255),   13),
-            'Gray':                 ((124, 124, 124),   14),
-            'Black':                ((0, 0, 0),         15),
+            'White':                ((255, 255, 255),   15),
+            'Gray':                 ((124, 124, 124),   16),
+            'Black':                ((0, 0, 0),         17),
         }
 
     def predict(self, image_file, image_size=32, min_score=0.005):
         image = Image.open(image_file)
-        image = image.resize((1000, 1000), Image.BICUBIC)  # Remove sensor noise/grain
-        image = image.resize((image_size, image_size), Image.NEAREST)  # Get the interesting colors without muddying them
+        image = image.resize((image_size, image_size), Image.BICUBIC)
         pixels = np.asarray(image)
         pixels = [j for i in pixels for j in i]
 
@@ -74,7 +68,7 @@ class ColorModel:
         # Colors are list of 3 floats (RGB) from 0.0 to 1.0
         a_h, a_s, a_v = rgb_to_hsv(a[0] / 255, a[1] / 255, a[2] / 255)
         b_h, b_s, b_v = rgb_to_hsv(b[0] / 255, b[1] / 255, b[2] / 255)
-        diff_h = 1 - abs(a_h - b_h)
+        diff_h = 1 - abs(a_h - b_h)  # Hue is more highly weighted than saturation and value
         diff_s = 1 - abs(a_s - b_s) * 0.5
         diff_v = 1 - abs(a_v - b_v) * 0.25
         score = diff_h * diff_s * diff_v
