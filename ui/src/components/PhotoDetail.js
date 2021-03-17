48ed667c0ae57bcd1b3073e14a51f3fa7de0c8ab
Gyan P <Gyanp@github.com>
2021-03-17 14:57:14 +0530

    display loading spinner until photo is completely loaded

--- ui/src/components/PhotoDetail.js
+++ ui/src/components/PhotoDetail.js
@@ -9,6 +9,7 @@ import ColorTags from './ColorTags'
 import HierarchicalTagsContainer from '../containers/HierarchicalTagsContainer'
 import EditableTags from '../components/EditableTags'
 import ImageHistogram from '../components/ImageHistogram'
+import Spinner from '../components/Spinner'
 import StarRating from './StarRating'
 import { PHOTO_UPDATE } from '../graphql/photo'
 
@@ -21,6 +22,7 @@ import '../static/css/PhotoDetail.css'
 
 const PhotoDetail = ({ photoId, photo, refetch }) => {
   const [starRating, updateStarRating] = useState(photo.starRating)
+  const [loading, setLoading] = useState(true)
   const [editorMode, setEditorMode] = useState(false)
   const [showBoundingBox, setShowBoundingBox] = useLocalStorageState(
     'showObjectBoxes',
@@ -77,170 +79,183 @@ const PhotoDetail = ({ photoId, photo, refetch }) => {
     var date = new Date(photo.takenAt)
     date = new Intl.DateTimeFormat().format(date)
   }
+  const handleImageLoaded = () => {
+    if (loading) {
+      setTimeout(() => {
+        setLoading(false)
+      }, 50)
+    }
+  }
+  const image = new Image()
+  image.onload = handleImageLoaded()
+  image.src = `/thumbnails/3840x3840_contain_q75/${photoId}/`
 
-  return (
-    <div
-      className="PhotoDetail"
-      style={{
-        backgroundImage: `url('/thumbnails/3840x3840_contain_q75/${photoId}/')`,
-      }}
-    >
-      <div className="content">
-        <div className="metadata">
-          <div className="boxes">
-            <div className="box">
-              <StarRating
-                starRating={starRating}
-                onStarClick={onStarClick}
-                large={true}
-                alwaysShow={true}
-              />
-              <div className="histogram">
-                <ImageHistogram
-                  imageUrl={`/thumbnails/3840x3840_contain_q75/${photoId}/`}
-                />
-              </div>
-            </div>
-            <div className="box">
-              <h2>Camera</h2>
-              <ul>
-                {photo.camera ? (
-                  <li>
-                    {photo.camera.make} {photo.camera.model}
-                  </li>
-                ) : (
-                  ''
-                )}
-                {date ? <li>Date: {date}</li> : ''}
-                <li>Aperture: {photo.aperture}</li>
-                <li>Exposure: {photo.exposure}</li>
-                <li>ISO speed: {photo.isoSpeed}</li>
-                <li>Focal length: {photo.focalLength}</li>
-                <li>Flash: {photo.flash ? 'ON' : 'OFF'}</li>
-                <li>Metering mode: {photo.meteringMode}</li>
-                {photo.driveMode ? <li>Drive mode: {photo.driveMode}</li> : ''}
-                {photo.shootingMode ? (
-                  <li>Shooting mode: {photo.shootingMode}</li>
-                ) : (
-                  ''
-                )}
-              </ul>
-            </div>
-            {photo.locationTags.length ? (
+  if (loading) return <Spinner />
+  if (!loading) {
+    return (
+      <div
+        className="PhotoDetail"
+        style={{
+          backgroundImage: `url('${image.src}')`,
+        }}
+      >
+        <div className="content">
+          <div className="metadata">
+            <div className="boxes">
               <div className="box">
-                <h2>Locations</h2>
-                <HierarchicalTagsContainer
-                  tags={photo.locationTags.map((item) => {
-                    let newItem = item.tag
-                    newItem.parent = item.parent
-                    return newItem
-                  })}
+                <StarRating
+                  starRating={starRating}
+                  onStarClick={onStarClick}
+                  large={true}
+                  alwaysShow={true}
                 />
-              </div>
-            ) : (
-              ''
-            )}
-            {photo.location ? (
-              <div className="box">
-                <h2>Map</h2>
-                <div className="map">
-                  {
-                    <MapView
-                      location={location}
-                      hideAttribution={true}
-                      zoom={6}
-                    />
-                  }
+                <div className="histogram">
+                  <ImageHistogram
+                    imageUrl={`/thumbnails/3840x3840_contain_q75/${photoId}/`}
+                  />
                 </div>
               </div>
-            ) : (
-              ''
-            )}
-            {photo.colorTags.length ? (
               <div className="box">
-                <h2>Colors</h2>
-                <ColorTags
-                  tags={photo.colorTags.map((item) => ({
-                    name: item.tag.name,
-                    significance: item.significance,
-                  }))}
-                />
+                <h2>Camera</h2>
+                <ul>
+                  {photo.camera ? (
+                    <li>
+                      {photo.camera.make} {photo.camera.model}
+                    </li>
+                  ) : (
+                    ''
+                  )}
+                  {date ? <li>Date: {date}</li> : ''}
+                  <li>Aperture: {photo.aperture}</li>
+                  <li>Exposure: {photo.exposure}</li>
+                  <li>ISO speed: {photo.isoSpeed}</li>
+                  <li>Focal length: {photo.focalLength}</li>
+                  <li>Flash: {photo.flash ? 'ON' : 'OFF'}</li>
+                  <li>Metering mode: {photo.meteringMode}</li>
+                  {photo.driveMode ? <li>Drive mode: {photo.driveMode}</li> : ''}
+                  {photo.shootingMode ? (
+                    <li>Shooting mode: {photo.shootingMode}</li>
+                  ) : (
+                    ''
+                  )}
+                </ul>
               </div>
-            ) : (
-              ''
-            )}
-            {photo.objectTags.length ? (
+              {photo.locationTags.length ? (
+                <div className="box">
+                  <h2>Locations</h2>
+                  <HierarchicalTagsContainer
+                    tags={photo.locationTags.map((item) => {
+                      let newItem = item.tag
+                      newItem.parent = item.parent
+                      return newItem
+                    })}
+                  />
+                </div>
+              ) : (
+                ''
+              )}
+              {photo.location ? (
+                <div className="box">
+                  <h2>Map</h2>
+                  <div className="map">
+                    {
+                      <MapView
+                        location={location}
+                        hideAttribution={true}
+                        zoom={6}
+                      />
+                    }
+                  </div>
+                </div>
+              ) : (
+                ''
+              )}
+              {photo.colorTags.length ? (
+                <div className="box">
+                  <h2>Colors</h2>
+                  <ColorTags
+                    tags={photo.colorTags.map((item) => ({
+                      name: item.tag.name,
+                      significance: item.significance,
+                    }))}
+                  />
+                </div>
+              ) : (
+                ''
+              )}
+              {photo.objectTags.length ? (
+                <div className="box">
+                  <h2>
+                    Objects
+                    {showBoundingBox ? (
+                      <VisibilityIcon
+                        onClick={() => updateboundingBoxVisibility(false)}
+                      />
+                    ) : (
+                      <VisibilityOffIcon
+                        onClick={() => updateboundingBoxVisibility(true)}
+                      />
+                    )}
+                  </h2>
+                  <ul>
+                    {photo.objectTags.map((photoTag, index) => (
+                      <li key={index}>{photoTag.tag.name}</li>
+                    ))}
+                  </ul>
+                </div>
+              ) : (
+                ''
+              )}
+              {photo.styleTags.length ? (
+                <div className="box">
+                  <h2>Styles</h2>
+                  <ul>
+                    {photo.styleTags.map((photoTag, index) => (
+                      <li key={index}>{photoTag.tag.name}</li>
+                    ))}
+                  </ul>
+                </div>
+              ) : (
+                ''
+              )}
               <div className="box">
                 <h2>
-                  Objects
-                  {showBoundingBox ? (
-                    <VisibilityIcon
-                      onClick={() => updateboundingBoxVisibility(false)}
-                    />
-                  ) : (
-                    <VisibilityOffIcon
-                      onClick={() => updateboundingBoxVisibility(true)}
-                    />
-                  )}
+                  Tags
+                  <EditIcon
+                    alt="Edit"
+                    onClick={() => setEditorMode(!editorMode)}
+                  />
                 </h2>
-                <ul>
-                  {photo.objectTags.map((photoTag, index) => (
-                    <li key={index}>{photoTag.tag.name}</li>
-                  ))}
-                </ul>
-              </div>
-            ) : (
-              ''
-            )}
-            {photo.styleTags.length ? (
-              <div className="box">
-                <h2>Styles</h2>
-                <ul>
-                  {photo.styleTags.map((photoTag, index) => (
-                    <li key={index}>{photoTag.tag.name}</li>
-                  ))}
-                </ul>
-              </div>
-            ) : (
-              ''
-            )}
-            <div className="box">
-              <h2>
-                Tags
-                <EditIcon
-                  alt="Edit"
-                  onClick={() => setEditorMode(!editorMode)}
+                <EditableTags
+                  tags={photo.genericTags}
+                  editorMode={editorMode}
+                  photoId={photoId}
+                  refetch={refetch}
                 />
-              </h2>
-              <EditableTags
-                tags={photo.genericTags}
-                editorMode={editorMode}
-                photoId={photoId}
-                refetch={refetch}
-              />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      {showBoundingBox && (
-        <div className="boundingBoxesContainer">
-          <BoundingBoxes
-            photoWidth={photo.width}
-            photoHeight={photo.height}
-            boxes={boxes}
-          />
+        {showBoundingBox && (
+          <div className="boundingBoxesContainer">
+            <BoundingBoxes
+              photoWidth={photo.width}
+              photoHeight={photo.height}
+              boxes={boxes}
+            />
+          </div>
+        )}
+        <div className="backIcon" title="[Esc] key to go back to photo list">
+          <ArrowBackIcon alt="Close" onClick={history.goBack} />
         </div>
-      )}
-      <div className="backIcon" title="[Esc] key to go back to photo list">
-        <ArrowBackIcon alt="Close" onClick={history.goBack} />
+        <div className="scrollHint">
+          <ArrowDownIcon className="img1" alt="" />
+          <ArrowDownIcon className="img2" alt="" />
+          <ArrowDownIcon className="img3" alt="" />
+        </div>
       </div>
-      <div className="scrollHint">
-        <ArrowDownIcon className="img1" alt="" />
-        <ArrowDownIcon className="img2" alt="" />
-        <ArrowDownIcon className="img3" alt="" />
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default PhotoDetail
