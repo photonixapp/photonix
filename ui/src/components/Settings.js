f5512d4d2f03197acfb703440339d3e9980a24a5
GyanP <GyanP@github.com>
2021-07-15 18:25:51 +0530

    resolved error 'useRef not defined' after resolving conflicts

--- ui/src/components/Settings.js
+++ ui/src/components/Settings.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { useQuery, useMutation } from '@apollo/client'
 import { useSelector } from 'react-redux'
 import { getActiveLibrary } from '../stores/libraries/selector'
