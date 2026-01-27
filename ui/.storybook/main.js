ccb88a0e265910e8af7565eb6bdfd9cbdc4d8f52
Damian Moore <damian@epixstudios.co.uk>
2026-01-27 20:24:14 +0000

    Storybook fixes

--- ui/.storybook/main.js
+++ ui/.storybook/main.js
@@ -11,4 +11,27 @@ module.exports = {
   docs: {
     autodocs: true,
   },
+  webpackFinal: async (config) => {
+    // Find and remove the existing SVG rule
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'))
+    if (fileLoaderRule) {
+      fileLoaderRule.exclude = /\.svg$/
+    }
+
+    // Add SVGR loader for SVG files
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            exportType: 'named',
+          },
+        },
+        'url-loader',
+      ],
+    })
+
+    return config
+  },
 }
